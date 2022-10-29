07783a8d33ce70780797e9afbd79c45da82e1ecd
ᴅᴍɪᴛʀʏ ᴇᴠɢᴇɴɪᴇᴠɪᴄʜ <keweegen@gmail.com>
2022-10-29 22:01:33 +0600

    rename fn NewLogger -> New

--- cmd/root.go
+++ cmd/root.go
@@ -1,41 +1,41 @@
 package cmd
 
 import (
-    "github.com/keweegen/notification/config"
-    "github.com/keweegen/notification/logger"
-    "github.com/spf13/cobra"
-    "log"
+	"github.com/keweegen/notification/config"
+	"github.com/keweegen/notification/logger"
+	"github.com/spf13/cobra"
+	"log"
 )
 
 var (
-    l   logger.Logger
-    cfg *config.Config
+	l   logger.Logger
+	cfg *config.Config
 )
 
 var rootCmd = &cobra.Command{
-    Use: "notification-service",
+	Use: "notification-service",
 }
 
 func Execute() error {
-    return rootCmd.Execute()
+	return rootCmd.Execute()
 }
 
 func init() {
-    cobra.OnInitialize(initLogger, initConfig)
+	cobra.OnInitialize(initLogger, initConfig)
 }
 
 func initLogger() {
-    var err error
-    l, err = logger.NewLogger()
-    if err != nil {
-        log.Fatalln("init logger", err)
-    }
+	var err error
+	l, err = logger.New()
+	if err != nil {
+		log.Fatalln("init logger", err)
+	}
 }
 
 func initConfig() {
-    var err error
-    cfg, err = config.Read()
-    if err != nil {
-        log.Fatalln("init config", err)
-    }
+	var err error
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalln("init config", err)
+	}
 }
